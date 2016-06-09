a6db5ab5a9daa089b2650a98baaabea3ea4563b5
Artem Mezin <a.mezin@corp.mail.ru>
2016-06-09 16:39:58 +0300

    Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,19 @@
-'use strict';
-
 const path = require('path');
 const load = require('load-grunt-config');
 const time = require('time-grunt');
 
 module.exports = function (grunt) {
-    time(grunt);
+	time(grunt);
 
-    var tasks = path.join(process.cwd(), 'tasks');
+	const tasks = path.join(process.cwd(), 'tasks');
 
-    load(grunt, {
-        configPath: tasks,
+	load(grunt, {
+		configPath: tasks,
 
-        jitGrunt: {
-            staticMappings: {
-                'test-runner': '@qa/grunt-test-runner'
-            }
-        }
-    });
+		jitGrunt: {
+			staticMappings: {
+				'test-runner': '@qa/grunt-test-runner',
+			},
+		},
+	});
 };
