b27104c4ed4c5b9d4c5e2742b18ff37738bb09b3
Alexander Abashkin <monolithed@gmail.com>
2016-06-22 15:23:44 +0300

    fixed gitattributes

--- tests/page.js
+++ tests/page.js
@@ -3,38 +3,38 @@
 let AccountManager = require('@qa/account-manager');
 
 class PageObject {
-    constructor () {
-        this.account = new AccountManager.Session();
-    }
+	constructor () {
+		this.account = new AccountManager.Session();
+	}
 
-    open (path) {
-        browser.url(path);
-    }
+	open (path) {
+		browser.url(path);
+	}
 
-    auth (type) {
-        let cookie = this.account.get('cookies', type);
+	auth (type) {
+		let cookie = this.account.get('cookies', type);
 
-        browser.url('/');
-        browser.setCookies(cookie);
-    }
+		browser.url('/');
+		browser.setCookies(cookie);
+	}
 
-    /**
-     * Combine selectors in CSS cascade
-     *
-     * @static
-     * @param  {Object} locators
-     * @param  {string} parent
-     * @return {Object}
-     */
-    static cssInherit (locators, parent) {
-        for (let name in locators) {
-            if (Object.prototype.hasOwnProperty.call(locators, name)) {
-                locators[name] = `${parent} ${locators[name]}`;
-            }
-        }
+	/**
+	 * Combine selectors in CSS cascade
+	 *
+	 * @static
+	 * @param  {Object} locators
+	 * @param  {string} parent
+	 * @return {Object}
+	 */
+	static cssInherit (locators, parent) {
+		for (let name in locators) {
+			if (Object.prototype.hasOwnProperty.call(locators, name)) {
+				locators[name] = `${parent} ${locators[name]}`;
+			}
+		}
 
-        return locators;
-    }
+		return locators;
+	}
 }
 
 module.exports = PageObject;
