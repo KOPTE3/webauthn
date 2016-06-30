ea843fe2c8a9fe67cb5be943ff908d4cc49973ec
Burlak Ilia <i.l.burlak@gmail.com>
2016-06-30 19:22:52 +0300

    [MAIL-51769] возвращенно количество инстансов 1

--- tests/e.mail.ru/config.js
+++ tests/e.mail.ru/config.js
@@ -43,7 +43,7 @@ exports.config = {
 	// connectionRetryTimeout: 10 * 1000,
 
 	/* Количество инстансов параллельного запуска тестов */
-	maxInstances: 10,
+	maxInstances: 1,
 
 	/** Использовать синхронное API */
 	// sync: true,
