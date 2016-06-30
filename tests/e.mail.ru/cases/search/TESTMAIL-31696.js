08e29b4ef55662b8858267ab5282830d51b80e6d
Vladimir Demidov <vladimir.dem@gmail.com>
2016-06-30 18:13:38 +0300

    [MAIL-50708] TESTMAIL-31696, 31695

---
+++ tests/e.mail.ru/cases/search/TESTMAIL-31696.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let Messages = require('../../steps/messages');
+let PortalSearchSteps = require('../../steps/portal-menu/portal-search');
+let AdvancedSteps = require('../../steps/portal-menu/advanced');
+
+describe('TESTMAIL-31696', () => {
+	it('Добавление операнда "непрочитанные" из расширенного поиска', () => {
+		Messages.auth();
+		Messages.open();
+		PortalSearchSteps.toggleAdvanced();
+
+		let name = 'unread';
+
+		AdvancedSteps.clickCheckbox(name);
+		PortalSearchSteps.hasOperand(name);
+		PortalSearchSteps.operandHasIcon(name);
+		PortalSearchSteps.operandHasClose(name);
+		AdvancedSteps.isVisible();
+		AdvancedSteps.isChecked(name);
+	});
+});
