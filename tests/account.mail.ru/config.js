f74dafd41eb50d4da8ec5e8af16b0c2b05042f31
Alexander Abashkin <monolithed@gmail.com>
2016-07-01 20:58:00 +0300

    fixed account config

--- tests/account.mail.ru/config.js
+++ tests/account.mail.ru/config.js
@@ -1,114 +1,33 @@
 'use strict';
 
-let AccountManager = require('@qa/account-manager'),
-	WebDriverAPI = require('@qa/wdio-api-mail.ru'),
-	TestTools = require('@qa/test-tools'),
-	capabilities = require('@qa/wd-capabilities');
+let TestTools = require('@qa/test-tools'),
+	capabilities = require('@qa/wd-capabilities'),
+	EMail = require('../e.mail.ru/config');
 
-let account = new AccountManager.Hooks();
 let support = new TestTools.Support();
-let project = 'tests/account.mail.ru';
+let project = 'account.mail.ru';
 
 /** @namespace browser **/
-exports.config = {
-	/*
-	 * Настройки с которыми запущен сервер Selenium
-	 *
-	 * Операционная система: Windows
-	 * Список доступных браузеров: Chrome, Firefox, Opera, IE 11
-	 * Грид: http://vagabond3.dev.mail.ru:4444/grid/console
-	 *
-	 * Операционная система: Linux
-	 * Список доступных браузеров: Chrome, Firefox
-	 * Грид: http://win110.dev.mail.ru:4444/grid/console
-	 */
-	host: 'localhost',
-	port: 4444,
-	path: '/wd/hub',
-
+exports.config = support.extend(EMail.config, {
 	/* Базовый адрес тестирования */
-	baseUrl: 'https://account.mail.ru',
-
-	/* Доступные значения: silent, verbose, command, data, result, error */
-	logLevel: 'silent',
-
-	/*
-	 * Максимальное время на выполнение команды.
-	 * Если какая-то из команд фреймворка не получит за это время результат,
-	 * то выполнение тестов будет прервано.
-	 */
-	waitforTimeout: 30 * 1000,
-
-	/* Максимальное время на выполнение повторного запроса. */
-	connectionRetryTimeout: 10 * 1000,
-
-	/* Количество инстансов параллельного запуска тестов */
-	maxInstances: 1,
-
-	/** Использовать синхронное API */
-	// sync: true,
-
-	/*
-	 * Опция позволяет отладчику остановить выполнение тестов
-	 * в месте вызова инструкции debugger.
-	 * Для использования этой опции требуется наличие пакета node-inspector
-	 */
-	debug: false,
-
-	/* Доступные значения: cucumber, mocha, jasmine */
-	framework: 'mocha',
-
-	mochaOpts: {
-		'ui': 'bdd',
-
-		/* Количество попыток на выполнение теста, который не был пройден */
-		'retries': 0,
-
-		/** Включить профилирование (сильно увеличивает время выполения тестов) */
-		// 'prof': true,
-
-		/** Максимальное время на ожидание результата выполнения теста */
-		'timeout': 10 * 1000,
-
-		/** Показывать стек-трейс */
-		'trace': true,
-		'full-trace': true,
-
-		/** Ругаться на вызов устаревших функций */
-		'throw-deprecation': true,
-		'trace-deprecation': true
-	},
-
-	/* Для реппортера Allure требуется наличие установленного плагина в CI */
-	reporters: ['dot', 'junit'],
+	baseUrl: `https://${project}`,
 
 	reporterOptions: {
-		outputDir: `./cache/${project}/reports`
+		outputDir: `./cache/tests/${project}/reports`
 	},
 
 	/* Директория, куда будут складываться скриншоты */
-	// screenshotPath: './cache/tests/shots',
+	// screenshotPath: `./cache/tests/${project}/shots`,
 
 	/* Директория, куда будут складываться логи */
-	logfile: `./cache/${project}/logs`,
-
-	/*
-	 * Список файлов с тестами.
-	 * Порядок файлов сохраняется, дубликаты исключаются
-	 */
-
-	/*
-	 * Список файлов, которые требуется запустить
-	 * Этот набор запускается всегда независимо от секции <suite>
-	 */
-	// specs: [ ],
+	logfile: `./cache/tests/${project}/logs`,
 
 	/**
 	 * Набор тестовых кейсов
 	 *
 	 * { <suite>: [ <files> ] }
 	 */
-	suites: support.suites(`${project}/cases`),
+	suites: support.suites(`tests/${project}/cases`),
 
 	/*
 	 * Обратие внимание на то, что браузеры запускаются параллельно
@@ -121,21 +40,5 @@ exports.config = {
 	 */
 	capabilities: [
 		capabilities.get('chrome')
-	],
-
-	before (capabilities, specs) {
-		let commands = new WebDriverAPI();
-
-		commands.export('all');
-	},
-
-	beforeSuite () {
-		return account.session({
-			host: browser.options.baseUrl
-		});
-	},
-
-	afterSuite () {
-		return account.discard();
-	}
-};
+	]
+});
