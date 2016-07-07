944cbfa15297077fca717640ed9911e819ac2ae5
Vladimir Demidov <vladimir.dem@gmail.com>
2016-07-07 12:56:03 +0300

    [MAIL-50708] refactor tests, fix failed test

--- tests/e.mail.ru/cases/search/TESTMAIL-31728.js
+++ tests/e.mail.ru/cases/search/TESTMAIL-31728.js
@@ -1,8 +1,10 @@
 'use strict';
 
 let Messages = require('../../steps/messages');
-let PortalSearchSteps = require('../../steps/portal-menu/portal-search');
-let AdvancedSteps = require('../../steps/portal-menu/advanced');
+let portalSearchSteps = require('../../steps/portal-menu/portal-search');
+let advancedSteps = require('../../steps/portal-menu/advanced');
+let CalendarFactory = require('../../utils/calendar');
+let advancedCalendar = CalendarFactory.create('advanced');
 
 let AdvancedStore = require('../../store/portal-menu/advanced');
 
@@ -14,26 +16,26 @@ describe('TESTMAIL-31728', () => {
 		Messages.auth();
 		Messages.open();
 
-		PortalSearchSteps.toggleAdvanced();
+		portalSearchSteps.toggleAdvanced();
 
 		let today = date.format('D.M.Y');
 		let todayDay = date.format('d');
 		let lapse = AdvancedStore.dateSelectValues.filter(({value}) => value === '1')[0];
 		let operandName = 'date';
 
-		AdvancedSteps.selectDateLapse(lapse.value);
-		AdvancedSteps.checkSelectDateText(lapse.text);
+		advancedSteps.selectDateLapse(lapse.value);
+		advancedSteps.checkSelectDateText(lapse.text);
 
-		AdvancedSteps.clickDateField();
-		AdvancedSteps.isCalendarVisible();
-		AdvancedSteps.calendarHasToday(todayDay);
+		advancedSteps.clickDateField();
+		advancedCalendar.isVisible();
+		advancedCalendar.hasToday(todayDay);
 
-		AdvancedSteps.clickCalendarToday();
-		AdvancedSteps.isCalendarVisible(true);
-		AdvancedSteps.checkDateFieldText(today);
+		advancedCalendar.clickToday();
+		advancedCalendar.isVisible(true);
+		advancedSteps.checkDateFieldText(today);
 
-		PortalSearchSteps.hasOperand(operandName);
-		PortalSearchSteps.checkOperandText(operandName, today);
-		PortalSearchSteps.checkDateOperandLapse(lapse.operandText);
+		portalSearchSteps.hasOperand(operandName);
+		portalSearchSteps.checkOperandText(operandName, today);
+		portalSearchSteps.checkDateOperandLapse(lapse.operandText);
 	});
 });
