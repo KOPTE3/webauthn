dbe79bedbd1e46a81bd9feb1a8ff36410af2682f
Burlak Ilia <i.l.burlak@gmail.com>
2016-07-08 18:06:25 +0300

    [MAIL-51769] fix test

--- tests/e.mail.ru/cases/compose/TESTMAIL-31541.js
+++ tests/e.mail.ru/cases/compose/TESTMAIL-31541.js
@@ -1,37 +1,37 @@
 'use strict';
 
-let Compose = require('../../steps/compose');
+let Messages = require('../../steps/messages');
 let composeFields = require('../../steps/compose/fields');
 let composeEditor = require('../../steps/compose/editor');
 let composeControls = require('../../steps/compose/controls');
+let SentPage = require('../../steps/sent');
 let composeEditorStore = require('../../store/compose/editor');
 let ComposeFieldsStore = require('../../store/compose/fields');
-let SentPage = require('../../steps/sent');
+let messagesToolbarSteps = require('../../steps/messages/toolbar');
 
-describe('TESTMAIL-31541: аписание письма. Забытое вложение. Проверить ' +
+describe('TESTMAIL-31541: AJAX аписание письма. Забытое вложение. Проверить ' +
 	'отсутствие попапа при отправке ' +
 	'(тексты для которых не должен появляться попап)', () => {
 	before(() => {
-		Compose.auth();
+		Messages.auth();
 	});
 
-	composeEditorStore.classifierTest.lettersWithoutAttach.forEach(function (text) {
-		it('Попап должен появится', () => {
-			let { fields } = new ComposeFieldsStore();
+	it('Попап должен появится', () => {
+		let { fields } = new ComposeFieldsStore();
 
-			Compose.features([
-				'check-missing-attach',
-				'disable-ballons',
-				'no-collectors-in-compose'
-			]);
+		Messages.features([
+			'check-missing-attach',
+			'disable-ballons',
+			'no-collectors-in-compose'
+		]);
 
-			Compose.open();
+		Messages.open();
+		messagesToolbarSteps.clickButton('compose');
 
-			composeFields.setFieldValue('subject', 'check attach');
-			composeFields.setFieldValue('to', fields.to);
-			composeEditor.writeMessage(text);
-			composeControls.send();
-			SentPage.isVisible();
-		});
+		composeFields.setFieldValue('subject', 'check attach');
+		composeFields.setFieldValue('to', fields.to);
+		composeEditor.writeMessage(composeEditorStore.texts.withoutAttach);
+		composeControls.send();
+		SentPage.isVisible();
 	});
 });
