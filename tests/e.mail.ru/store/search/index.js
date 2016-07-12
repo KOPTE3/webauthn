e14668bdced8d2f8a99fc83b58a66987d990fba9
Vladimir Demidov <vladimir.dem@gmail.com>
2016-07-12 21:27:11 +0300

    [MAIL-50708] TESTMAIL-31746

--- tests/e.mail.ru/store/search/index.js
+++ tests/e.mail.ru/store/search/index.js
@@ -29,5 +29,36 @@ module.exports = {
 				query: 'test'
 			}
 		];
+	},
+
+	get twoRequests () {
+		return [
+			{
+				flags: {
+					attach: true
+				},
+				correspondents: {
+					from: 'test1@mail.ru'
+				}
+			},
+			{
+				flags: {
+					unread: true,
+					flagged: true
+				},
+				subject: 'test',
+				correspondents: {
+					from: 'test3@mail.ru',
+					to: 'test2@mail.ru'
+				}
+			}
+		];
+	},
+
+	get twoRequestsSuggests () {
+		return [
+			'От: test1@mail.ru с вложениями',
+			'От: test3@mail.ru кому: test2@mail.ru с темой: test непрочитанные отмеченные флажком'
+		];
 	}
 };
