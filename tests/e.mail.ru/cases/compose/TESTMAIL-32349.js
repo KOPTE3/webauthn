86473670c7e89614ad97f2ef7f3fe9cdf8b9e253
Marina Titova <una.ladrona@gmail.com>
2016-07-13 16:08:00 +0300

    TESTMAIL-32349

---
+++ tests/e.mail.ru/cases/compose/TESTMAIL-32349.js
@@ -0,0 +1,63 @@
+'use strict';
+
+let Message = require('../../steps/message');
+let Messages = require('../../steps/messages');
+let lettersSteps = require('../../steps/messages/letters');
+let composeControls = require('../../steps/compose/controls');
+let composeFields = require('../../steps/compose/fields');
+let ComposeFieldsStore = require('../../store/compose/fields');
+let actions = require('../../utils/actions');
+let messageToolbarSteps = require('../../steps/message/toolbar');
+let SentPage = require('../../steps/sent');
+
+let composeEditorStore = require('../../store/compose/editor');
+
+const Compose = require('../../steps/compose');
+
+// mail
+let Mail = require('../../utils/mail');
+
+const subject = 'TESTMAIL-32349';
+const features = [
+	'check-missing-attach',
+	'disable-ballons',
+	'no-collectors-in-compose',
+	'disable-fastreply-landmark'
+];
+
+describe('TESTMAIL-32349: Из НЕ AJAX чтения. ' +
+	'Ответ на письмо. Забытое вложение. ' +
+	'Проверить отсутствие попапа на полной пересылке ' +
+	'(текст, для которого попап не должен появляться)', () => {
+	before(() => {
+		Messages.auth();
+	});
+
+	it('письмо должно быть успешно отправлено', () => {
+		let {fields} = new ComposeFieldsStore();
+
+		var mail = new Mail({
+			to: fields.to,
+			subject,
+			text: composeEditorStore.texts.withoutAttach
+		});
+
+		mail.send();
+
+		Messages.features(features);
+		Messages.open();
+		lettersSteps.openNewestLetter();
+
+		Message.wait();
+
+		messageToolbarSteps.clickButton('forward');
+
+		Compose.wait();
+
+		composeFields.setFieldValue('to', fields.to);
+
+		composeControls.send();
+
+		SentPage.wait();
+	});
+});
