dfff043f82b8360d3bb555df1eefebada665876b
Marina Titova <una.ladrona@gmail.com>
2016-07-14 17:47:57 +0300

    TESTMAIL-31530

---
+++ tests/e.mail.ru/cases/password/restore/TESTMAIL-31530.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let AccessSteps = require('../../../steps/password/restore/select');
+let usersStore = require('../../../store/password/restore/users');
+let passwordStore = require('../../../store/password/restore');
+
+let accessSteps = new AccessSteps();
+let {symbols} = passwordStore;
+
+describe.skip('TESTMAIL-31530: mrim. Вериф телефон. ' +
+	'Ввод скрытых цифр телефона.', () => {
+	beforeEach(() => {
+		accessSteps.open(usersStore.mrim.one);
+	});
+
+	it('Проверить ввод кириллицы, латиницы и спецсимволов с клавиатуры', () => {
+		accessSteps.fillPhoneInput(symbols.cyrillic);
+		accessSteps.checkPhoneInput('');
+	});
+
+	it('Проверить ввод латиницы', () => {
+		accessSteps.fillPhoneInput(symbols.latin);
+		accessSteps.checkPhoneInput('');
+	});
+
+	it('Проверить ввод специальных символов', () => {
+		symbols.special.forEach(symbol => {
+			accessSteps.fillPhoneInput(symbol);
+			accessSteps.checkPhoneInput('');
+		});
+	});
+});
