c59f77ebe001e33557c883d75f0a1c935fc4a978
Vladimir Demidov <vladimir.dem@gmail.com>
2016-07-20 18:55:23 +0300

    [MAIL-50047] TESTMAIL-32342

---
+++ tests/e.mail.ru/cases/search/MAIL-50047/TESTMAIL-32342.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let Messages = require('../../../steps/messages');
+let PortalSearchSteps = require('../../../steps/portal-menu/portal-search');
+let AdvancedSteps = require('../../../steps/portal-menu/advanced');
+let portalSearchSteps = new PortalSearchSteps();
+let advancedSteps = new AdvancedSteps();
+
+let advancedStore = require('../../../store/portal-menu/advanced');
+
+const text = 'test';
+
+describe('TESTMAIL-32342: Поиск. Новые операнды. Проверка, что при вводе данных ' +
+	'в расширенном поиске они сразу отображаются в быстром поиске.', () => {
+	before(() => {
+		Messages.auth();
+		Messages.open();
+
+		portalSearchSteps.toggleAdvanced();
+	});
+
+	advancedStore.textFields.forEach(name => {
+		it(name, () => {
+			advancedSteps.setFieldText(name, text);
+			portalSearchSteps.hasOperand(name);
+			portalSearchSteps.checkOperandText(name, text);
+		});
+	});
+});
