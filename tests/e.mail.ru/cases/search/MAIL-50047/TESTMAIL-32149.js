1977aa421d4fa424b1d50ac8e67b74232729bc4f
Vladimir Demidov <vladimir.dem@gmail.com>
2016-07-21 13:47:10 +0300

    [MAIL-50047] TESTMAIL-32149

---
+++ tests/e.mail.ru/cases/search/MAIL-50047/TESTMAIL-32149.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let Messages = require('../../../steps/messages');
+let PortalSearchSteps = require('../../../steps/portal-menu/portal-search');
+let portalSearchSteps = new PortalSearchSteps();
+
+const email = 'test@mail.ru';
+
+describe('TESTMAIL-32149: Поиск. Новые операнды.', () => {
+	before(() => {
+		Messages.auth();
+		Messages.open();
+
+		portalSearchSteps.clickSearchField();
+		portalSearchSteps.setOperandText('blank', 'from:');
+		portalSearchSteps.setOperandText('from', email);
+	});
+
+	it('Поиск. Новые операнды. Проверка, что при смене операндов от и кому ' +
+		'не мигает аватарка.', () => {
+		portalSearchSteps.clickOperandDropdown('from');
+		portalSearchSteps.operandNoAvatar('from');
+		portalSearchSteps.clickOperandDropdownItem('from', 'to');
+		portalSearchSteps.hasOperand('to');
+		portalSearchSteps.operandHasAvatar('to');
+		portalSearchSteps.checkOperandAvatar('to', email);
+	});
+});
