f4fcad0f62adcc26fde38dfafd0bb9bb88dd5260
Marina Titova <una.ladrona@gmail.com>
2016-07-27 15:07:39 +0300

    TESTMAIL-31524

--- tests/e.mail.ru/cases/password/restore/TESTMAIL-31476.js
+++ tests/e.mail.ru/cases/password/restore/TESTMAIL-31476.js
@@ -1,29 +1,51 @@
 'use strict';
 
+let path = require('path');
+
 let AccountSteps = require('../../../steps/password/restore/account');
-let SelectSteps = require('../../../steps/password/restore/select');
+let AccessSteps = require('../../../steps/password/restore/access');
+let SelectSteps = require('../../../steps/password/restore/');
 let RecoverySteps = require('../../../steps/password/restore/recovery');
-let usersStore = require('../../../store/password/restore/users');
+let phoneStore = require('../../../store/phones');
 
 let accountSteps = new AccountSteps();
 let selectSteps = new SelectSteps();
+let accessSteps = new AccessSteps();
 
-describe('TESTMAIL-31476: Восстановление пароля. ' +
-	'Ввод скрытых цифр телефона.', () => {
-	it('Проверить ввод некорректных цифр', () => {
-		const user = usersStore.simple.one;
-		const incorrect = usersStore.getIncorrectValue(user.phone.value);
-		let captcha;
+let name = path.basename((module.parent.options ? module.parent : module).filename, '.js');
 
+let {options = {
+	name: 'Восстановление пароля. ' +
+	'Ввод скрытых цифр телефона. ' +
+	'Проверить ввод некорректных цифр'
+}} = module.parent;
+
+let user = {};
+let steps = options.mrim ? accessSteps : selectSteps;
+
+
+describe(name, () => {
+	before(() => {
+		user = AccessSteps.addUser({
+			phones: 1,
+			mrim: options.mrim
+		});
+	});
+
+	beforeEach(() => {
 		accountSteps.openForEmail(user.email);
-		selectSteps.wait();
+		steps.wait();
+	});
 
-		captcha = selectSteps.getPhoneCaptchaValue();
-		selectSteps.fillPhoneInput(incorrect);
-		selectSteps.fillPhoneCaptcha(captcha);
-		selectSteps.submitForm();
+	it(options.name, () => {
+		let incorrect = phoneStore.getIncorrectValue(user.phones[0].value);
+		let captcha = steps.getPhoneCaptchaValue();
 
-		selectSteps.checkTabError('Вы указали неправильный номер телефона');
-		selectSteps.checkPhoneCaptcha(captcha, false);
+		steps.fillPhoneInput(incorrect);
+		steps.fillPhoneCaptcha(captcha);
+		steps.submitForm();
+
+		steps.checkTabError('Вы указали неправильный номер телефона');
+		steps.checkPhoneCaptcha(captcha, false);
 	});
 });
