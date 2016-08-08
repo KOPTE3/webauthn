e594d3d299800cdec941988c3936df2177675461
Vladimir Demidov <vladimir.dem@gmail.com>
2016-08-08 19:44:17 +0300

    [MAIL-49673] TESTMAIL-32942

---
+++ tests/e.mail.ru/cases/compose/MAIL-49673/TESTMAIL-32942.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let { options = {
+	name: 'Написание письма. Имена файлов в теме письма. Проверка, что если ' +
+	'сами очистили тему, то при добавлении файла его имя не добавляется в тему письма'
+}} = module.parent;
+
+let composeFolder = options.compose2 ? 'compose2' : 'compose';
+
+let Compose = require(`../../../steps/${composeFolder}`);
+let Messages = require('../../../steps/messages');
+
+let MessagesToolbarSteps = require('../../../steps/messages/toolbar');
+let messagesToolbar = new MessagesToolbarSteps();
+
+let ComposeFieldsSteps = require(`../../../steps/${composeFolder}/fields`);
+let composeFields = new ComposeFieldsSteps();
+
+let ComposeAttachesSteps = require(`../../../steps/${composeFolder}/attaches`);
+let composeAttaches = new ComposeAttachesSteps();
+
+const filename = 'test.txt';
+const filename2 = 'test1.png';
+
+describe(() => {
+	before(() => {
+		Compose.auth();
+
+		if (options.compose2) {
+			Messages.features([
+				'compose2'
+			]);
+		}
+
+		if (options.noajax) {
+			Compose.open();
+		} else {
+			Messages.open();
+			messagesToolbar.clickButton('compose');
+			Compose.wait();
+		}
+	});
+
+	it(options.name, () => {
+		composeFields.checkFieldValue('subject', '');
+
+		composeAttaches.uploadAttach(filename);
+		composeAttaches.hasAttach(filename);
+		composeFields.checkFieldValue('subject', filename);
+
+		composeFields.setFieldValue('subject', '');
+
+		composeAttaches.uploadAttach(filename2);
+		composeAttaches.hasAttach(filename2);
+		composeFields.checkFieldValue('subject', '');
+	});
+});
