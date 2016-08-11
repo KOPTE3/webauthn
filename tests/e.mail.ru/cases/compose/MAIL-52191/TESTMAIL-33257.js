3a82cc21560f80bb8a64b3b4fd01cebbcecf316d
Marina Titova <una.ladrona@gmail.com>
2016-08-11 15:30:40 +0300

    Clean up TESTMAIL-33257 TESTMAIL-33271

--- tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33257.js
+++ tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33257.js
@@ -8,39 +8,29 @@ let { options = {
 
 let composeFolder = options.compose2 ? 'compose2' : 'compose';
 
-let Compose = require(`../../../steps/${composeFolder}`);
 let Messages = require('../../../steps/messages');
-let Message = require('../../../steps/message');
+let MessageToolbarSteps = require('../../../steps/message/toolbar');
+let LettersSteps = require('../../../steps/messages/letters');
 let SentPage = require('../../../steps/sent');
 
-let LettersSteps = require('../../../steps/messages/letters');
-let letters = new LettersSteps();
-
-let MessageToolbarSteps = require('../../../steps/message/toolbar');
-let messageToolbar = new MessageToolbarSteps();
-
-let MessagesToolbarSteps = require('../../../steps/messages/toolbar');
-let messagesToolbar = new MessagesToolbarSteps();
-
+let Compose = require(`../../../steps/${composeFolder}`);
 let ComposeControlsSteps = require(`../../../steps/${composeFolder}/controls`);
-let composeControls = new ComposeControlsSteps();
-
 let ComposeFieldsSteps = require(`../../../steps/${composeFolder}/fields`);
-let composeFields = new ComposeFieldsSteps();
-
 let ComposeEditorSteps = require(`../../../steps/${composeFolder}/editor`);
-let composeEditor = new ComposeEditorSteps();
-
 let ComposeReplyReattachLayer = require('../../../steps/layers/compose/replyReattach');
-let replyReattachLayer = new ComposeReplyReattachLayer();
 
-let composeFieldsStore = require('../../../store/compose/fields');
 let authStore = require('../../../store/authorization');
+let composeFieldsStore = require('../../../store/compose/fields');
 let composeEditorStore = require('../../../store/compose/editor');
 
 let Mail = require('../../../utils/mail');
-let messagesUtils = require('../../../utils/messages');
-let messageUtils = require('../../../utils/message');
+
+let letters = new LettersSteps();
+let messageToolbar = new MessageToolbarSteps();
+let composeFields = new ComposeFieldsSteps();
+let composeEditor = new ComposeEditorSteps();
+let composeControls = new ComposeControlsSteps();
+let replyReattachLayer = new ComposeReplyReattachLayer();
 
 const attach = 'file3.txt';
 
--- tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33271.js
+++ tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33271.js
@@ -8,44 +8,19 @@ let { options = {
 let composeFolder = options.compose2 ? 'compose2' : 'compose';
 
 let Compose = require(`../../../steps/${composeFolder}`);
-let Messages = require('../../../steps/messages');
-let Message = require('../../../steps/message');
+let ComposeFieldsSteps = require(`../../../steps/${composeFolder}/fields`);
+let ComposeEditorSteps = require(`../../../steps/${composeFolder}/editor`);
+let ComposeControlsSteps = require(`../../../steps/${composeFolder}/controls`);
 let SentPage = require('../../../steps/sent');
 
-let LettersSteps = require('../../../steps/messages/letters');
-let letters = new LettersSteps();
-
-let MessageToolbarSteps = require('../../../steps/message/toolbar');
-let messageToolbar = new MessageToolbarSteps();
-
-let MessagesToolbarSteps = require('../../../steps/messages/toolbar');
-let messagesToolbar = new MessagesToolbarSteps();
-
-let ComposeControlsSteps = require(`../../../steps/${composeFolder}/controls`);
-let composeControls = new ComposeControlsSteps();
-
-let ComposeFieldsSteps = require(`../../../steps/${composeFolder}/fields`);
-let composeFields = new ComposeFieldsSteps();
-
-let ComposeEditorSteps = require(`../../../steps/${composeFolder}/editor`);
-let composeEditor = new ComposeEditorSteps();
-
-let ComposeReplyReattachLayer = require('../../../steps/layers/compose/replyReattach');
-let replyReattachLayer = new ComposeReplyReattachLayer();
-
 let composeFieldsStore = require('../../../store/compose/fields');
-let authStore = require('../../../store/authorization');
 let composeEditorStore = require('../../../store/compose/editor');
-
-let Mail = require('../../../utils/mail');
-let messagesUtils = require('../../../utils/messages');
-let messageUtils = require('../../../utils/message');
-
 let actions = require('../../../utils/actions');
 
-const attach = 'file3.txt';
+let composeFields = new ComposeFieldsSteps();
+let composeEditor = new ComposeEditorSteps();
+let composeControls = new ComposeControlsSteps();
 
-let anotherAccount;
 
 describe(() => {
 	before(() => {
