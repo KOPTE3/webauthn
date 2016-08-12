c0ad0c5023fc55b972ac451f9fbbab9236e52788
Marina Titova <una.ladrona@gmail.com>
2016-08-12 14:38:47 +0300

    Meta tests on broken attach

--- tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33286.js
+++ tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33286.js
@@ -1,10 +1,6 @@
 'use strict';
 
-let { options = {
-	name: 'НЕ AJAX. Ответ на письмо. Забытое вложение. ' +
-	'Проверить появление попапа на полном ответе ' +
-	'если в письме был заблокирован аттач'
-}} = module.parent;
+let { options = {}} = module.parent;
 
 let composeFolder = options.compose2 ? 'compose2' : 'compose';
 
@@ -14,48 +10,37 @@ let MessageToolbarSteps = require('../../../steps/message/toolbar');
 let MessageFastreplySteps = require('../../../steps/message/fastreply');
 let LettersSteps = require('../../../steps/messages/letters');
 
-
 let Compose = require(`../../../steps/${composeFolder}`);
 let ComposeControlsSteps = require(`../../../steps/${composeFolder}/controls`);
-let ComposeFieldsSteps = require(`../../../steps/${composeFolder}/fields`);
 let ComposeEditorSteps = require(`../../../steps/${composeFolder}/editor`);
-let ComposeAttachesSteps = require(`../../../steps/${composeFolder}/attaches`);
-
-let MissingAttachLayer = require('../../../steps/layers/missingAttach');
-let NotifySteps = require('../../../steps/notify');
 
 let authStore = require('../../../store/authorization');
 let composeFieldsStore = require('../../../store/compose/fields');
-let composeEditorStore = require('../../../store/compose/editor');
 
 let Mail = require('../../../utils/mail');
 
-let notify = new NotifySteps();
 let letters = new LettersSteps();
 let messageToolbar = new MessageToolbarSteps();
 let messageFastreply = new MessageFastreplySteps();
-let composeFields = new ComposeFieldsSteps();
 let composeEditor = new ComposeEditorSteps();
 let composeControls = new ComposeControlsSteps();
-let composeAttaches = new ComposeAttachesSteps();
-let missingAttachLayer = new MissingAttachLayer();
 
-let filename = 'broken.scr';
-let anotherAccount;
+let features = [
+	'reattach-to-reply',
+	'check-missing-attach',
+	'disable-ballons',
+	'no-collectors-in-compose',
+	'disable-fastreply-landmark'
+];
+let fields, user, email;
+
+let brokenAttachMeta = require('./meta/brokenAttach');
 
 describe(() => {
 	before(() => {
 		Messages.auth();
 
-		let features = [
-			'reattach-to-reply',
-			'check-missing-attach',
-			'disable-ballons',
-			'no-collectors-in-compose',
-			'disable-fastreply-landmark'
-		];
-
-		let { fields } = composeFieldsStore;
+		fields = composeFieldsStore.fields;
 		let mail = new Mail({
 			to: fields.to,
 			subject: fields.subject,
@@ -64,44 +49,74 @@ describe(() => {
 
 		mail.send();
 
-		Messages.features(features);
-		Messages.open();
-
-		letters.waitForNewestLetter();
-		letters.openNewestLetter();
-
-		if (options.fastReply) {
-			Message.refresh();
-			messageFastreply.clickButton('reply');
-			composeEditor.wait();
-		} else {
-			messageToolbar.clickButton('reply');
-			Compose.wait();
-			Compose.refresh();
-		}
-
-		anotherAccount = authStore.credentials();
+		user = authStore.credentials();
+		email = `${user.login}@${user.domain}`;
 	});
 
-	it(options.name, () => {
-		composeFields.setFieldValue('to',
-			`${anotherAccount.login}@${anotherAccount.domain}`);
-		composeEditor.writeMessage(composeEditorStore.texts.withAttach);
-		composeAttaches.uploadAttachWithoutCheck(filename);
-		notify.wait('error', `Файл ${filename} заблокирован в целях безопасности.`, 'contains');
+	describe('TESTMAIL-33286', () => {
+		it('НЕ AJAX: Быстрый ответ', () => {
+			brokenAttachMeta({
+				email,
+				open () {
+					Messages.features(features);
+					Messages.open();
+					letters.openBySubject(fields.subject);
+					Message.refresh();
+					messageFastreply.clickButton('reply');
+					composeEditor.wait();
+				},
+				send () {
+					messageToolbar.clickFastreplyButton('reply');
+				},
+				cancel () {
+					messageToolbar.clickFastreplyButton('cancel');
+				}
+			});
+		});
+	});
 
-		if (options.fastReply) {
-			messageToolbar.clickFastreplyButton('reply');
-		} else {
-			composeControls.send();
-		}
+	describe('TESTMAIL-33283', () => {
+		it('НЕ AJAX: Полный ответ', () => {
+			brokenAttachMeta({
+				email,
+				open () {
+					Messages.features(features);
+					Messages.open();
+					letters.openBySubject(fields.subject);
+					messageToolbar.clickButton('reply');
+					Compose.wait();
+					Compose.refresh();
+				},
+				send () {
+					composeControls.send();
+				},
+				cancel () {
+					composeControls.cancel();
+				}
+			});
+		});
+	});
 
-		missingAttachLayer.wait();
-		missingAttachLayer.checkTexts();
+	describe('TESTMAIL-33256', () => {
+		it('Написание', () => {
+			brokenAttachMeta({
+				email,
+				open () {
+					Compose.features(features);
+					Compose.open();
+				},
+				send () {
+					composeControls.send();
+				},
+				cancel () {
+					composeControls.cancel();
+				}
+			});
+		});
 	});
 
 	after(() => {
-		authStore.discard(anotherAccount.id).catch(() => {
+		authStore.discard(user.id).catch(() => {
 			throw new Error('Не освободили аккаунт после себя!');
 		});
 	});
