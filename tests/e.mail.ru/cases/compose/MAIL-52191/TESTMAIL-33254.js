3738fa89999fba437f1405716a0f28f18bcd2a81
Marina Titova <una.ladrona@gmail.com>
2016-08-15 18:20:43 +0300

    TESTMAIL-33254 betatest

--- tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33254.js
+++ tests/e.mail.ru/cases/compose/MAIL-52191/TESTMAIL-33254.js
@@ -1,8 +1,9 @@
 'use strict';
 
 let { options = {
-	name: 'AJAX. Написание письма. Забытое вложение. ' +
-	'Проверить отсутствие попапа в письме с инлайн аттачем'
+	name: 'НЕ AJAX. Ответ на письмо. Забытое вложение. ' +
+	'Проверить отсутствие попапа для быстрой пересылки ' +
+	'с текстом в цитате, с аттачем (исходное письмо с аттачем)'
 }} = module.parent;
 
 let composeFolder = 'compose2';
@@ -14,13 +15,23 @@ let ComposeAttachesSteps = require(`../../../steps/${composeFolder}/attaches`);
 let ComposeControlsSteps = require(`../../../steps/${composeFolder}/controls`);
 let SentPage = require('../../../steps/sent');
 
+let Messages = require('../../../steps/messages');
+let Login = require('../../../steps/login');
+let LoginFormSteps = require('../../../steps/login/form');
+
 let composeEditorStore = require('../../../store/compose/editor');
-let composeFieldsStore = require('../../../store/compose/fields');
 
 let composeFields = new ComposeFieldsSteps();
 let composeEditor = new ComposeEditorSteps();
 let composeAttaches = new ComposeAttachesSteps();
 let composeControls = new ComposeControlsSteps();
+
+let loginFormSteps = new LoginFormSteps();
+
+let user = {
+	email: 'ok_nez135@mail.ru',
+	password: 'qwerty@6'
+};
 
 describe(() => {
 	before(() => {
@@ -34,19 +45,23 @@ describe(() => {
 			'compose2-inlinefromeditor'
 		];
 
-		Compose.auth();
+		Login.open();
+		loginFormSteps.send({
+			Login: user.email,
+			Password: user.password
+		});
+		Messages.wait();
+
 		Compose.features(features);
 		Compose.open();
 	});
 
 	it(options.name, () => {
-		const { fields } = composeFieldsStore;
 		const filename = 'pic.jpg';
 
 		composeAttaches.registerAddLogger();
 
-		composeFields.setFieldValue('to', fields.to);
-		composeFields.setFieldValue('subject', fields.subject);
+		composeFields.setFieldValue('to', user.email);
 		composeEditor.writeMessage(composeEditorStore.texts.withAttach);
 
 		composeAttaches.attachInline(filename);
