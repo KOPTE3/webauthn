54e611adae3461e492d31a430efe03137fda17a0
Vladimir Demidov <vladimir.dem@gmail.com>
2016-08-17 16:32:59 +0300

    [MAIL-51938] TESTMAIL-32961

---
+++ tests/e.mail.ru/cases/compose/MAIL-51938/TESTMAIL-32961.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let Signature = require('../../../steps/settings/signature');
+let Compose2EditorSteps = require('../../../steps/compose2/editor');
+
+let compose2Editor = new Compose2EditorSteps();
+
+const filenames = ['jpg.jpg', 'test1.png', 'file30.gif'];
+
+describe(() => {
+	before(() => {
+		Signature.auth();
+
+		Signature.features([
+			'wysiwyg-signature',
+			'wysiwyg-signature-inline-images',
+			'compose2-inlinefromeditor'
+		]);
+	});
+
+	beforeEach(() => {
+		Signature.open();
+		Signature.hasWysiwyg();
+	});
+
+	filenames.forEach((filename) => {
+		it('Настройки. HTML подпись. Проверка работы кнопки ' +
+			'Вставить картинку (несколько картинок, формат jpg, png, gif): ' + filename, () => {
+			Signature.attachInline(filename);
+			compose2Editor.hasInline();
+		});
+	});
+
+});
