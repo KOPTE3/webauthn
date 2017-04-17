a7315a2c70d1d79013de9cfbdbc1de868dcfd3a1
Alexander Abashkin <monolithed@gmail.com>
2017-04-17 19:00:48 +0300

    Added utils/transport

---
+++ utils/transport.ts
@@ -0,0 +1,135 @@
+import * as assert from 'assert';
+import authorization from '@qa/yoda/store/authorization';
+import RPC from '@qa/rpc';
+import { MailAPI } from '@qa/api';
+import system from '@qa/yoda/store/system';
+import * as merge from 'deepmerge';
+
+import {
+	Deliveryd,
+	IDeliverydData,
+	IDeliverydBody,
+	IDeliverydRequest,
+	IDeliverydResponse,
+	IAPIResponse,
+	API
+} from '@qa/transport';
+
+type RequiredInternal<T, K extends keyof T> = { [P in K]: T[P] };
+type Required<T> = T & RequiredInternal<T, keyof T>;
+
+interface ITransportData {
+	subject?: string,
+	content?: string,
+}
+
+export type ISendMessageData = MailAPI.MessagesSend & ITransportData;
+
+export interface IPutMessageData {
+	folder?: number;
+	subject?: string;
+	content?: string | Buffer;
+	attachments?: Array<string>;
+	raw?: IDeliverydBody;
+	type?: string;
+	headers?: {
+		[name: string]: string;
+	}
+}
+
+class Transport {
+	data: ITransportData
+
+	constructor (data?: ITransportData) {
+		this.data = {
+			subject: 'Test subject',
+			content: 'Test content',
+			...data
+		};
+	}
+
+	/**
+	 * Покладка письма в текущий ящик
+	 *
+	 * @see test/msgput
+	 *
+	 * {
+	 *   subject,
+	 *   content,
+	 *   ...
+	 * }
+	 */
+	putMessage (params: IPutMessageData): Promise<IDeliverydResponse> {
+		let { attachments = [] } = params;
+		let { username, password } = authorization.account.data();
+
+		let transport = new Deliveryd({ username, password });
+
+		return browser.waitForPromise(async () => {
+			let { subject, content: body }: Required<IPutMessageData> = { ...this.data, ...params };
+
+			// По умолчанию используем папку "Входящие"
+			let { folder = 0 } = params;
+
+			// Приводим список файлов к формату транспорта
+			let files = attachments.map(value => {
+				return {
+					name: value,
+					path: system.file(value)
+				};
+			});
+
+			let response = await transport.send({
+				subject,
+				body,
+				...{ params, ...files }
+				},
+				{ folder }
+			);
+
+			assert.equal(response.status, RPC.HTTPStatus.OK);
+
+			return response;
+		});
+	}
+
+	/**
+	 * Отправка письма на сторонний текущий ящик
+	 *
+	 * @see http://api.tornado.dev.mail.ru/messages/send
+	 *
+	 * {
+	 *   subject,
+	 *   content,
+	 *   ...
+	 * }
+	 */
+	sendMessage (params: ISendMessageData): Promise<IAPIResponse> {
+		let { username, password } = authorization.account.data();
+
+		let request = new API({ username, password });
+
+		return browser.waitForPromise(async () => {
+			let { subject, content }: Required<IPutMessageData> = { ...this.data, ...params };
+
+			params = merge<ISendMessageData>({
+				subject,
+				body: {
+					html: content
+				},
+				correspondents: {
+					to: username
+				}
+			}, params);
+
+			let response = await request.send(params);
+
+			assert.equal(response.status, RPC.HTTPStatus.OK);
+
+			return response;
+		}, null, 'Could not send message');
+	}
+}
+
+export { IDeliverydData, IDeliverydRequest, IDeliverydResponse };
+export default Transport;
