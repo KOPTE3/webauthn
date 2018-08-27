655c1dc85717cd38f0d27544e0c6595436d3d7a9
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2018-08-27 23:34:27 +0300

    Prepare commit

--- test.ts
+++
@@ -1,9 +0,0 @@
-import 'core-js';
-import 'reflect-metadata';
-
-import * as path from 'path';
-import gen from './tasks/gen';
-
-
-gen({root: path.resolve(__dirname, 'element')})
-	.catch(console.error);
