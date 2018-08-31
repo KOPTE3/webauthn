254f1dcfe18e18d8f38268f24a9a5b38c752e486
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2018-08-31 17:06:41 +0300

    New API description

--- api/internal/tokens-info.ts
+++ api/internal/tokens-info.ts
@@ -1,15 +1,26 @@
 import call, {callAsync, RequestResult} from './call';
 
 
-type Id = {restore_id: string} | {id: string};
+type Options = {email: string} & ({restore_id: string} | {id: string}) & {update?: Partial<TokensInfo>};
+
+/**
+ * неполный интерфейс, можно и нужно дополнить необходимыми полями
+ */
+export interface TokensInfo {
+	transport: string;
+	email: string;
+	redirect_uri: string;
+	method: string;
+	code: string;
+}
 
 /**
  * @see http://api.tornado.dev.mail.ru/test/tokens/info
  */
-export default function tokensInfo (options: {email: string} & Id): RequestResult<string> {
+export default function tokensInfo (options: Options): RequestResult<TokensInfo> {
 	return call('test/tokens/info', options);
 }
 
-export async function tokensInfoAsync (options: {email: string} & Id): Promise<RequestResult<string>> {
+export async function tokensInfoAsync (options: Options): Promise<RequestResult<TokensInfo>> {
 	return callAsync('test/tokens/info', options);
 }
