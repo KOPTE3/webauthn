ed36df792c4123021e4214b4de69069c58457519
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-03 16:24:07 +0300

    fixed mistakes

--- api/mail-api/ab-contacts-add.ts
+++ api/mail-api/ab-contacts-add.ts
@@ -5,10 +5,10 @@ import call, { callAsync } from './call';
 /**
  * @see http://api.tornado.dev.mail.ru/ab/contacts/add
  */
-export default function userShort (options: MailAPI.AbContactsAdd): RequestResult<Array<string>> {
+export default function abContactsAdd (options: MailAPI.AbContactsAdd): RequestResult<Array<string>> {
 	return call('ab/contacts/add', options);
 }
 
-export async function userShortAsync (options: MailAPI.AbContactsAdd): Promise<RequestResult<Array<string>>> {
+export async function abContactsAddAsync (options: MailAPI.AbContactsAdd): Promise<RequestResult<Array<string>>> {
 	return callAsync('ab/contacts/add', options);
 }
--- api/mail-api/user-edit.ts
+++ api/mail-api/user-edit.ts
@@ -5,10 +5,10 @@ import call, { callAsync } from './call';
 /**
  * @see http://api.tornado.dev.mail.ru/user/edit
  */
-export default function userShort (options: MailAPI.UserEdit): RequestResult<void> {
+export default function userEdit (options: MailAPI.UserEdit): RequestResult<void> {
 	return call('user/edit', options);
 }
 
-export async function userShortAsync (options: MailAPI.UserEdit): Promise<RequestResult<void>> {
+export async function userEditAsync (options: MailAPI.UserEdit): Promise<RequestResult<void>> {
 	return callAsync('user/edit', options);
 }
