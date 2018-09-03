267c04e77ec4ff5c63691eec3576d1a497f6ba3f
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-03 15:43:00 +0300

    removed blank lines

--- api/mail-api/folders-add.ts
+++ api/mail-api/folders-add.ts
@@ -2,7 +2,6 @@ import { MailAPI } from '@qa/api';
 import { RequestResult } from '../../types/api';
 import call, { callAsync } from './call';
 
-
 /**
  * @see http://api.tornado.dev.mail.ru/folders/add
  */
