6b2e97590cf5471d1462d6ae6abff630f59a59ca
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-03 15:23:11 +0300

    folders/open

---
+++ api/mail-api/folders-open.ts
@@ -0,0 +1,15 @@
+import { MailAPI } from '@qa/api';
+import { RequestResult } from '../../types/api';
+import call, { callAsync } from './call';
+
+
+/**
+ * @see http://api.tornado.dev.mail.ru/folders/open
+ */
+export default function foldersAdd (options: MailAPI.FoldersOpen): RequestResult<string[]> {
+	return call('folders/open', options);
+}
+
+export async function foldersAddAsync (options: MailAPI.FoldersOpen): Promise<RequestResult<string[]>> {
+	return callAsync('folders/open', options);
+}
