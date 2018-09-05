89e28d30bb4ea0c18ebec2ccd803d3a89d65eea7
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-05 18:47:21 +0300

    grouped internal methods

--- api/internal/index.ts
+++ api/internal/index.ts
@@ -1,10 +1,7 @@
 export {default as call, callAsync} from './call';
+export * from './user';
+export * from './bruteforce-counter';
+export * from './users';
 export {default as config, configAsync} from './config';
-export {default as userAdditionalEmailChange, userAdditionalEmailChangeAsync} from './user-additional-email-change';
-export {default as userPhonesState, userPhonesStateAsync} from './user-phones-state';
-export {default as usersPhonesAdd, usersPhonesAddAsync} from './users-phones-add';
-export {default as tokensInfo, tokensInfoAsync} from './tokens-info';
-export {default as bruteforceCounterReset, bruteforceCounterResetAsync} from './bruteforce-counter-reset';
-export {default as bruteforceCounterEmu, bruteforceCounterEmuAsync} from './bruteforce-counter-emu';
-export {default as usersEdit, usersEditAsync} from './users-edit';
 export {default as messagesAttachesInvalidate, messagesAttachesInvalidateAsync} from './messages-attaches-invalidate';
+export {default as tokensInfo, tokensInfoAsync} from './tokens-info';
