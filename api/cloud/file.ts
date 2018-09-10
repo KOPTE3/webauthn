ea7a17b8b618018fcfbb7a903412a5c8c8f6f4f8
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-10 19:54:08 +0300

    file method

---
+++ api/cloud/file.ts
@@ -0,0 +1,36 @@
+import { Credentials, RequestResult } from '../../types/api';
+import call, { callAsync } from './call';
+
+interface Options {
+	home: string;
+}
+
+interface FileResponseBody {
+	count?: { // if "home" field refers to folder
+		folders: number,
+		files: number
+	};
+	home: string | { // string if 200, object if 404
+		value: string
+		error: string
+	};
+
+	[key: string]: any;
+}
+
+/**
+ * @see http://docs.i.munkin.devmail.ru/cloud-api/master/_docs/file.html
+ */
+export default function file(
+	options: Options,
+	credentials?: Credentials
+): RequestResult<FileResponseBody> {
+	return call('file', options, 'GET', credentials);
+}
+
+export async function fileAsync(
+	options: Options,
+	credentials?: Credentials
+): Promise<RequestResult<FileResponseBody>> {
+	return callAsync('file', options, 'GET', credentials);
+}
