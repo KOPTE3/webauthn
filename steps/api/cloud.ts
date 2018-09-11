5d6142c928147e91e3b4465bbf671f3c5bd6f51b
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-11 11:31:12 +0300

    initial cloud api steps

---
+++ steps/api/cloud.ts
@@ -0,0 +1,13 @@
+import * as assert from 'assert';
+import * as CloudApi from '../../api/cloud';
+
+export default class CloudApiSteps {
+	@step('Проверить, что файл {filePath} {reverse ? "" : "не "}существует в Облаке')
+	checkFileExists(filePath: string, reverse: boolean = false) {
+		assert.equal(
+			CloudApi.file({ home: filePath }).body.home.error,
+			reverse,
+			`Файл "${filePath}" ${reverse ? 'при' : 'от'}сутствует в Облаке`
+		);
+	}
+}
