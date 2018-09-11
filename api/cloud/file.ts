e5f5d4fbcec69bcf6764774d90b5052faa4d6ff4
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-11 15:41:35 +0300

    @qa/api used to provide interfaces

--- api/cloud/file.ts
+++ api/cloud/file.ts
@@ -1,10 +1,6 @@
 import { Credentials, RequestResult } from '../../types/api';
 import call, { callAsync } from './call';
-
-interface Options {
-	home?: string;
-	weblink?: string;
-}
+import { CloudAPI } from '@qa/api';
 
 interface FileResponseBody {
 	count?: { // if "home" field refers to folder
@@ -23,14 +19,14 @@ interface FileResponseBody {
  * @see http://docs.i.munkin.devmail.ru/cloud-api/master/_docs/file.html
  */
 export default function file(
-	options: Options,
+	options: CloudAPI.File,
 	credentials?: Credentials
 ): RequestResult<FileResponseBody> {
 	return call('file', options, 'GET', credentials, true);
 }
 
 export async function fileAsync(
-	options: Options,
+	options: CloudAPI.File,
 	credentials?: Credentials
 ): Promise<RequestResult<FileResponseBody>> {
 	return callAsync('file', options, 'GET', credentials, true);
