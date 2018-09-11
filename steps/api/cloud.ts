769c2a82edcb68bc4b64c669623d3c7870e2d098
Yury Baykov <y.baykov@corp.mail.ru>
2018-09-11 16:05:17 +0300

    clearCloud() step

--- steps/api/cloud.ts
+++ steps/api/cloud.ts
@@ -2,6 +2,20 @@ import * as assert from 'assert';
 import * as CloudApi from '../../api/cloud';
 
 export default class CloudApiSteps {
+	/**
+	 * 1) Получает список файлов и папок в корневой директории Облака
+	 * 2) Удалят каждый(ую) из них - перемещает в корзину
+	 * 3) Очищает корзину
+	 */
+	@step('Очистить Облако от файлов')
+	clearCloud() {
+		const rootFolderFiles: string[] = CloudApi.folder({ home: '/' }).body.list.map(({ home }) => home);
+
+		rootFolderFiles.forEach((filePath: string) => CloudApi.fileRemove({ home: filePath }));
+
+		CloudApi.trashbinEmpty();
+	}
+
 	@step('Проверить, что файл {filePath} {reverse ? "" : "не "}существует в Облаке')
 	checkFileExists(filePath: string, reverse: boolean = false) {
 		assert.ok(
