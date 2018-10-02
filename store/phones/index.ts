8c3c31cdacd6caf9062368143cbbec7bb2b06f8e
Yury Baykov <y.baykov@corp.mail.ru>
2018-10-02 20:18:27 +0300

    filled test phones ids

--- store/phones/index.ts
+++ store/phones/index.ts
@@ -52,7 +52,7 @@ export default {
 			value: '01',
 			masked: '+7 (985) 101-**-**',
 			full: '+7 (985) 101-77-20',
-			id: '',
+			id: 'id_ejzdMW603/dDmpnW7dKf+ZPuhStkdVPSq+OgPl9wNPY=',
 			operator: Operators.MTS
 		},
 		{
@@ -62,7 +62,7 @@ export default {
 			value: '36',
 			masked: '+7 (926) 236-**-**',
 			full: '+7 (926) 236-17-85',
-			id: '',
+			id: 'id_16TLfv1RUmqxc2Q0owULC7vBF952pPI2dj4+G3TY4N0=',
 			operator: Operators.MEGAFON
 		},
 		{
@@ -72,7 +72,7 @@ export default {
 			value: '39',
 			masked: '+7 (926) 239-**-**',
 			full: '+7 (926) 239-90-73',
-			id: '',
+			id: 'id_5v4tDsAhbWIqigK8dmUsGDtIPi8Gs4uyWEBBzTVkln8=',
 			operator: Operators.MEGAFON
 		},
 		{
@@ -82,7 +82,7 @@ export default {
 			value: '29',
 			masked: '+7 (926) 529-**-**',
 			full: '+7 (926) 529-95-51',
-			id: '',
+			id: 'id_HE48M+yRv/dtupq3WxUavdV3aOIaLTLJf0QVlz0A3Fk=',
 			operator: Operators.MEGAFON
 		}
 	] as PhoneList,
