2bc16b7c2b2558538eb022cf78b1975731a0caf3
Sergey Volodin <s.volodin@corp.mail.ru>
2019-01-23 11:27:07 +0300

    AC-746: fixed signature

--- api/internal/user/2-step-enable.ts
+++ api/internal/user/2-step-enable.ts
@@ -6,15 +6,19 @@ interface Option {
 	phone_id: string;
 }
 
+interface Options {
+	users: Option[];
+}
+
 const method = 'users/2-step-auth/enable';
 
 /**
  * @see http://api.tornado.dev.mail.ru/users/2-step-auth/enable
  */
-export default function twoStepEnable(options: Option[]): RequestResult<any> {
+export default function twoStepEnable(options: Options): RequestResult<any> {
 	return call(method, options);
 }
 
-export async function twoStepEnableAsync(options: Option[]): Promise<RequestResult<any>> {
+export async function twoStepEnableAsync(options: Options): Promise<RequestResult<any>> {
 	return callAsync(method, options);
 }
