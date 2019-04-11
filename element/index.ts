8b37f62cd6118790eaf2c5c1e0a70ddee3551d9b
y.golubev <y.golubev@corp.mail.ru>
2019-04-11 18:46:32 +0300

    OCTAVIUS-2190: * todo

--- element/index.ts
+++ element/index.ts
@@ -390,6 +390,7 @@ export class Element {
 		Browser.LeftButtonUp();
 	}
 
+	// TODO: пофиксить @gen для методов с двумя элементами
 	@gen
 	@step('Перетащить элемент {element} на элемент {targetElement}')
 	static DragTo(element: Element, targetElement: Element, dragX?: number, dragY?: number) {
