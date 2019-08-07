1108aedaa1c99739a01833d465ba37d6b9fd45cb
Yury Baykov <y.baykov@corp.mail.ru>
2019-08-07 20:18:01 +0300

    fixed message/set_msg_props params type

--- api/internal/message-set_msg_props.ts
+++ api/internal/message-set_msg_props.ts
@@ -2,17 +2,19 @@ import { MailAPI } from '@qa/api';
 import { RequestResult } from '../../types/api';
 import call, { callAsync } from './call';
 
+type TestMessageSetMsgPropsFixed = MailAPI.TestMessageSetMsgProps & { message_id: string };
+
 /**
  * http://api.tornado.dev.mail.ru/test/message/set_msg_props
  */
 export default function messageSetMsgProps(
-	params: MailAPI.TestMessageSetMsgProps
+	params: TestMessageSetMsgPropsFixed
 ): RequestResult<string[]> {
 	return call('test/message/set_msg_props', params, 'POST');
 }
 
 export async function messageSetMsgPropsAsync(
-	params: MailAPI.TestMessageSetMsgProps
+	params: TestMessageSetMsgPropsFixed
 ): Promise<RequestResult<string[]>> {
 	return callAsync('test/message/set_msg_props', params, 'POST');
 }
