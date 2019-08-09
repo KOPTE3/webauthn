d16dfbf85ffc027cfff208685e318c5b0838e642
Yury Baykov <y.baykov@corp.mail.ru>
2019-08-09 19:20:32 +0300

    collected all helpers/ methods to folder

---
+++ api/mail-api/helpers/index.ts
@@ -0,0 +1,2 @@
+export { default as helpersUpdate, helpersUpdateAsync } from './helpers-update';
+export { default as helpersRemove, helpersRemoveAsync } from './helpers-remove';
--- api/mail-api/index.ts
+++ api/mail-api/index.ts
@@ -7,9 +7,8 @@ export * from './filters';
 export * from './threads';
 export * from './addressbook/labels';
 export * from './messages';
+export * from './helpers';
 export { default as abContactsAdd, abContactsAddAsync } from './ab-contacts-add';
-export { default as helpersUpdate, helpersUpdateAsync } from './helpers-update';
-export { default as helpersRemove, helpersRemoveAsync } from './helpers-remove';
 export { default as tokensSend, tokensSendAsync } from './tokens-send';
 export {
 	default as paymentMessagesStatusUpdate,
