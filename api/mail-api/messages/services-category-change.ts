57a1cfafd0a480223c0cceaddfccb86b0f96e1f0
Yury Baykov <y.baykov@corp.mail.ru>
2019-08-09 19:36:05 +0300

    categories specified

--- api/mail-api/messages/services-category-change.ts
+++ api/mail-api/messages/services-category-change.ts
@@ -1,20 +1,25 @@
 import { MailAPI } from '@qa/api';
 import { Credentials, RequestResult } from '../../../types/api';
 import call, { callAsync } from '../call';
+import { Merge } from 'type-fest';
 
 /**
  * @see http://api.tornado.dev.mail.ru/messages/services/category/change
  */
 
+// уточняем возможные значения поля category
+type Categories = 'travel' | 'event' | 'finance' | 'registration' | 'order' | 'fees';
+type MessagesServicesCategoryChange = Merge<MailAPI.MessagesServicesCategoryChange, { category: Categories }>;
+
 export default function messagesServicesCategoryChange(
-	options: MailAPI.MessagesServicesCategoryChange,
+	options: MessagesServicesCategoryChange,
 	credentials?: Credentials
 ): RequestResult<string[]> {
 	return call('messages/services/category/change', options, 'POST', credentials);
 }
 
 export function messagesServicesCategoryChangeAsync(
-	options: MailAPI.MessagesServicesCategoryChange,
+	options: MessagesServicesCategoryChange,
 	credentials?: Credentials
 ): Promise<RequestResult<string[]>> {
 	return callAsync('messages/services/category/change', options, 'POST', credentials);
