fd95395604332bf7939dcef7b295e29d8d605d99
Yury Baykov <y.baykov@corp.mail.ru>
2019-10-24 18:46:13 +0300

    post

--- api/internal/payment-history-insert.ts
+++ api/internal/payment-history-insert.ts
@@ -5,7 +5,7 @@ import { RequireExactlyOne } from 'type-fest';
 interface SourceItem {
 	date?: number; // default - текущий таймстемп
 	status: 'new' | 'success' | 'awaiting' | 'error';
-	receiver: string;
+	provider: string;
 	description: string; // или description_parts
 	description_parts: Array<{ // или description
 		field: string,
@@ -27,9 +27,9 @@ interface Options {
  * @see https://apidoc.devmail.ru/e.mail.ru/test/payment/history/insert
  */
 export default function paymentHistoryInsert(options: Options): RequestResult<void> {
-	return call('test/payment/history/insert', options);
+	return call('test/payment/history/insert', options, 'POST');
 }
 
 export async function paymentHistoryInsertAsync(options: Options): Promise<RequestResult<void>> {
-	return callAsync('test/payment/history/insert', options);
+	return callAsync('test/payment/history/insert', options, 'POST');
 }
