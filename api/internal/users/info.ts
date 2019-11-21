16483ec15391949142c2cabb7c1eb3e9e1e4614e
Morozov Ivan <iv.morozov@corp.mail.ru>
2019-11-21 15:17:42 +0300

    FMAIL-4538: fix review

--- api/internal/users/info.ts
+++ api/internal/users/info.ts
@@ -13,10 +13,10 @@ export interface UserInfoBody {
 /**
  * @see https://apidoc.devmail.ru/e.mail.ru/users/
  */
-export default function getUserInfo(userInfo: {emails: string[]}): RequestResult<UserInfoBody> {
+export default function Users(userInfo: {emails: string[]}): RequestResult<UserInfoBody> {
 	return call('users', userInfo, 'POST');
 }
 
-export async function getUserInfoAsync(userInfo: {emails: string[]}): Promise<RequestResult<UserInfoBody>> {
+export async function UsersAsync(userInfo: {emails: string[]}): Promise<RequestResult<UserInfoBody>> {
 	return callAsync('users', userInfo, 'POST');
 }
